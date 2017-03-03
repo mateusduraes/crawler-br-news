8b2bace37ac5874bbd023df9d53741d7a25defac
mateusduraes <mduraes1994@gmail.com>
2017-03-03 14:00:07 -0300

    Getting news from Estado de Minas

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 const requestPromise = require('request-promise')
 const cheerio = require('cheerio')
+
+let getNews = html => {
+	let $ = cheerio.load(html)
+	let arr = []
+	$('[data-destaque="titulo"]')
+		.each((index, item) => arr.push({
+			title: $(item).text(),
+			link : $(item).closest('[data-destaque="link"]').attr('href')
+		}))
+	return arr
+}
+
+requestPromise('http://www.em.com.br/')
+	.then(getNews)
+	.then(console.log)
+
+	
+	
+
+
+
